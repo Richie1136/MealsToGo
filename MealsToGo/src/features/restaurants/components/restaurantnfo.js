711535e68f357831a41d11a48f44e2a0c278d381
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2023-01-09 09:28:10 -0500

    Converted all styles to be in styled components

--- MealsToGo/src/features/restaurants/components/restaurantnfo.js
+++ MealsToGo/src/features/restaurants/components/restaurantnfo.js
@@ -1,9 +1,16 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
 import { Card } from 'react-native-paper';
 import styled from 'styled-components/native';
 
 const Title = styled.Text`
 padding: 16px;
+`
+const RestaurantCard = styled(Card)`
+backgroundColor: white
+`
+const RestaurantCardCover = styled(Card.Cover)`
+backgroundColor: white;
+padding: 20px;
 `
 
 export const RestaurantInfo = ({ restaurant = {} }) => {
@@ -20,14 +27,9 @@ export const RestaurantInfo = ({ restaurant = {} }) => {
   } = restaurant
 
   return (
-    <Card elevation={5} style={styles.card}>
-      <Card.Cover key={name} style={styles.cover} source={{ uri: photos[0] }} />
+    <RestaurantCard elevation={5}>
+      <RestaurantCardCover key={name} source={{ uri: photos[0] }} />
       <Title>{name}</Title>
-    </Card>
+    </RestaurantCard>
   );
 }
-
-const styles = StyleSheet.create({
-  card: { backgroundColor: "white" },
-  cover: { padding: 20, backgroundColor: "white" },
-});
