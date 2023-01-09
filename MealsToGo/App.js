08c5d95af3b2c231e9998bd3e19d2ffe7e8065a6
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2023-01-09 10:13:12 -0500

    Created a theme provider for different colors spacing, sizes and fonts

--- MealsToGo/App.js
+++ MealsToGo/App.js
@@ -1,13 +1,18 @@
 import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
+import { ThemeProvider } from 'styled-components/native';
 import { RestaurantsScreen } from './src/features/screens/restaurantscreen';
+import { theme } from './src/infrastructure/theme';
 
 export default function App() {
 
 
   return (
     <>
-      <RestaurantsScreen />
+      <ThemeProvider theme={theme}>
+        <RestaurantsScreen />
+      </ThemeProvider>
       <ExpoStatusBar style="auto" />
+
     </>
   );
 }
